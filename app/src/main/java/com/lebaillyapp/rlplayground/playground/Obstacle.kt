8bf2b12e0f43c56ba2236a3c08d83ef491daf2db
Samuel Lebailly <sam.meusa@gmail.com>
2025-06-26 20:26:14 +0200

    feature(playground): Create an obstacle data model.

---
+++ app/src/main/java/com/lebaillyapp/rlplayground/playground/Obstacle.kt
@@ -0,0 +1,8 @@
+package com.lebaillyapp.rlplayground.playground
+
+import android.util.SizeF
+
+data class Obstacle(
+    val position: Vector2,  // coin haut-gauche
+    val size: Vector2              // largeur et hauteur
+)
